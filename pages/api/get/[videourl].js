c8917f027f0d6e3fc78b9af4126a4860986a1914
Railson OL <itsrayplay@gmail.com>
2021-02-22 19:26:16 -0300

    done

--- pages/api/get/[videourl].js
+++ pages/api/get/[videourl].js
@@ -9,7 +9,7 @@ const get = async (req, res) => {
             let options;
 
             const chromeExecPaths = {
-                win32: 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe',
+                win32: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe', //X86 use: C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe
                 linux: '/usr/bin/google-chrome',
                 darwin: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
             }
@@ -44,7 +44,7 @@ const get = async (req, res) => {
 
         await browser.close()
 
-        res.setHeader('Cache-Control', 's-maxage=72000000, stale-while-revalidate')
+        res.setHeader('Cache-Control', 's-maxage=72000000, stale-while-revalidate') // 20 hours cache in versel, not work in Localhost
         return res.redirect(301, url)
 
     } catch (error) {
