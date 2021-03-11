8a4e75a092a6eabbf9992025acd86c48fea4b4d0
Railson OL <itsrayplay@gmail.com>
2021-03-11 20:12:31 -0300

    Update [videourl].js

--- pages/api/get/[videourl].js
+++ pages/api/get/[videourl].js
@@ -5,15 +5,21 @@ const get = async (req, res) => {
         let videourl = encodeDecode(req.query.videourl, 'decode', 'base64')
         let response = ''
 
-        await axios.get(videourl, {
-          maxRedirects: 0
-        })
-          .then(function (res) {
-            response = res.response.headers.location
-          })
-          .catch((err) => {
-            response = err.response.headers.location
-          })
+        if (videourl.includes('pubfolder') || videourl.includes('videos.')) {
+
+            response = videourl
+            
+        }else{
+            await axios.get(videourl, {
+                maxRedirects: 0
+              })
+                .then(function (res) {
+                  response = res.response.headers.location
+                })
+                .catch((err) => {
+                  response = err.response.headers.location
+            })
+        }
       
         res.setHeader('Cache-Control', 's-maxage=84000, stale-while-revalidate')
         return res.redirect(301, response)
