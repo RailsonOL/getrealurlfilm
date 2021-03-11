a1f44f8e05e6218a46a23969ab464a7afb90223b
Railson OL <itsrayplay@gmail.com>
2021-03-11 20:00:32 -0300

    Update utils.js

--- utils/utils.js
+++ utils/utils.js
@@ -1,5 +1,3 @@
-const httpStatus = require('http-status-codes')
-
 const encodeDecode = (string, type = 'encode', encodeType = 'base64') => {
     if (type == 'encode') {
         const result = new Buffer.from(string).toString(encodeType)
@@ -10,22 +8,6 @@ const encodeDecode = (string, type = 'encode', encodeType = 'base64') => {
     }
 }
 
-const responseJson = (res, data, statusCode = httpStatus.OK) => {
-    res.setHeader('Cache-Control', 's-maxage=72000000, stale-while-revalidate')
-    res.status(200);
-    return res.json(data);
-}
-
-const responseErrorJson = (res, methodName, error, statusCode = httpStatus.INTERNAL_SERVER_ERROR) => {
-    res.status(error.httpCode || statusCode)
-    console.error(methodName, error.message)
-    return res.json({
-        error: error.message || error.toString()
-    })
-}
-
 module.exports = {
-    responseErrorJson,
-    responseJson,
     encodeDecode
 }
